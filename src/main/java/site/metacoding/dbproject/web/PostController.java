cc807c227c086d8a9dbfb35161e627625f0cea03
stuuuu <woasmsshfl@naver.com>
2022-03-15 16:18:55 +0900

    Post목록 페이징 완료

--- src/main/java/site/metacoding/dbproject/web/PostController.java
+++ src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.dbproject.domain.post.Post;
@@ -25,75 +29,84 @@ public class PostController {
     private final HttpSession session;
     private final PostRepository postRepository;
 
-    // GET 글쓰기 페이지 /post/writeForm - 인증 O
+    // GET 글쓰기 페이지 /post/writeForm - 인증 필요
     @GetMapping("/s/post/writeForm")
     public String writeForm() {
 
         if (session.getAttribute("principal") == null) {
             return "redirect:/loginForm";
         }
-
         return "post/writeForm";
     }
 
-    // 메인페이지 - 인증 X
-    // GET 글목록 페이지 /post/list, /
+    // 메인페이지
+    // GET 글목록 페이지 /post/list, / - 인증 필요x
     // @GetMapping({"/", "/post/list"})
     @GetMapping({ "/", "/post/list" })
-    public String list(Model model) {
+    public String list(@RequestParam(defaultValue = "0") Integer page, Model model) {
         // 1. postRepository의 findAll() 호출
         // 2. model에 담기
-        model.addAttribute("posts", postRepository.findAll(Sort.by(Sort.Direction.DESC, "id")));
+        // model.addAttribute("posts",
+        // postRepository.findAll(Sort.by(Sort.Direction.DESC, "id")));
+        PageRequest pq = PageRequest.of(page, 3);
+        model.addAttribute("posts", postRepository.findAll(pq));
+        model.addAttribute("prevPage", page - 1);
+        model.addAttribute("nextPage", page + 1);
         return "post/list";
     }
 
-    // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨) - 인증 X
-    @GetMapping("/post/{id}") // Get요청에 /post 제외 시키기
+    // @GetMapping("/test/post/list")
+    // public @ResponseBody Page<Post> llistTest(@RequestParam(defaultValue = "0")
+    // Integer page) {
+    // PageRequest pq = PageRequest.of(page, 3);
+    // return postRepository.findAll(pq);
+    // }
+
+    // GET 글상세보기 페이지 /post/{id} (삭제버튼, 수정버튼 만들어 두면됨) - 인증 필요 x
+    @GetMapping("/post/{id}") // get 요청에 /post 제외 시키기
     public String detail(@PathVariable Integer id, Model model) {
         Optional<Post> postOp = postRepository.findById(id);
 
         if (postOp.isPresent()) {
             Post postEntity = postOp.get();
             model.addAttribute("post", postEntity);
-            System.out.println("=========================================================");
+            System.out.println("================================");
             return "post/detail";
         } else {
             return "error/page1";
         }
-
     }
 
-    // GET 글수정 페이지 /post/{id}/updateForm - 인증 O
+    // GET 글수정 페이지 /post/{id}/updateForm - 인증 필요
     @GetMapping("/s/post/{id}/updateForm")
     public String updateForm(@PathVariable Integer id) {
         return "post/updateForm"; // ViewResolver 도움 받음.
     }
 
-    // DELETE 글삭제 /post/{id} - 글목록으로 가기 - 인증 O
+    // DELETE 글삭제 /post/{id} - 글목록으로 가기 - 인증 필요
     @DeleteMapping("/s/post/{id}")
     public String delete(@PathVariable Integer id) {
         return "redirect:/";
     }
 
-    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기 - 인증 O
+    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기 - 인증 필요
     @PutMapping("/s/post/{id}")
     public String update(@PathVariable Integer id) {
         return "redirect:/post/" + id;
     }
 
-    // POST 글쓰기 /post - 글목록으로 가기 - 인증 O
+    // POST 글쓰기 /post - 글목록으로 가기 - 인증 필요
     @PostMapping("/s/post")
     public String write(Post post) {
 
-        // title, content 1. null검사, 2.공백검사, 3.길이검사 .........
+        // title, content 1.null 검사 2.공백
 
         if (session.getAttribute("principal") == null) {
             return "redirect:/loginForm";
         }
-
         User principal = (User) session.getAttribute("principal");
         post.setUser(principal);
-        // insert into post(title, content, userId) values(사용자, 사용자, 세션오브젝트의 PK)
+        // insert into post(title,content,userId) values(사용자,사용자,세션오브젝트의 PK)
 
         postRepository.save(post);
         return "redirect:/";
